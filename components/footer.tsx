afd04f2407f2cd8dc671c5adc25ff7ca7a1fb49a
pandusudo <pandusudo@gmail.com>
2024-12-30 16:29:35 +0700

    fix: separator in the footer for mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { conf } from "@/lib/constant";
 export default function Footer() {
   return (
     <div className="mt-20">
-      <div className="max-w-3xl border-t-[1.5px] bg-transparent mx-auto border-t-muted-foreground"></div>
+      <div className="max-w-0 sm:max-w-3xl border-t-[1.5px] bg-transparent mx-auto border-t-muted-foreground"></div>
       <footer className="container mx-auto pt-3 pb-12 max-w-screen-lg">
         <p className="leading-7 font-[family-name:var(--font-geist-sans)] font-light text-foreground text-center mt-16">
           © 2025 Created by {conf.AUTHOR.NAME}.
